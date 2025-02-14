74788c11cd18216dcfc1099b30a9ed6391098060
diegosy12 <diegosy12@educastur.es>
2025-02-14 19:09:47 +0100

    Refactorizados metosdos

--- src/aritmetica/Repaso.java
+++ src/aritmetica/Repaso.java
@@ -9,68 +9,72 @@ public class Repaso {
 
     
     public static void main(String[] args) {
-
-        
         Scanner scanner = new Scanner(System.in);
 
-        
         System.out.println("\nSeleccione una figura para calcular su area:");
 
-        	System.out.println("1. Cuadrado");
+        System.out.println("1. Cuadrado");
 
-        	System.out.println("2. Circulo");
+        System.out.println("2. Circulo");
 
-        	System.out.println("3. Triangulo");
+        System.out.println("3. Triangulo");
 
-        	System.out.print("Elige una opcion: ");
+        System.out.print("Elige una opcion: ");
 
+        int opcion = scanner.nextInt();
 
-            
-    int opcion = scanner.nextInt();
+        switch (opcion) {
 
+            case 1:	
+                calcularAreaCuadrado(scanner);
+                break;
 
-            
-    switch (opcion) {
+            case 2:	
+                calcularAreaCirculo(scanner);
+                break;
+            case 3:	
+                calcularAreaTriangulo(scanner);
+                break;
 
-            		case 1:	System.out.print("Introduce el lado del cuadrado: ");
+            default:
+                System.out.println("Opcion no valida. Intentalo de nuevo.");
 
-                		double lado = scanner.nextDouble();
-	
-                	double areaCuadrado = lado * lado;
+        }
 
-                		System.out.println("El area de la figura es: " + areaCuadrado);
+    }
 
-                		break;
- 
-           		case 2:	System.out.print("Introduce el radio del circulo: ");
+    private static void calcularAreaTriangulo(Scanner scanner) {
+        System.out.print("Introduce la base del triangulo: ");
+        
+        double base = scanner.nextDouble();
+        
+        System.out.print("Introduce la altura del triangulo: ");
+        
+        double altura = scanner.nextDouble();
+        
+        double areaTriangulo = (base * altura) / 2;
+        
+        System.out.println("El area de la figura es: " + areaTriangulo);
+    }
 
-                		double radio = scanner.nextDouble();
+    private static void calcularAreaCirculo(Scanner scanner) {
+        System.out.print("Introduce el radio del circulo: ");
+        
+        double radio = scanner.nextDouble();
+        
+        double areaCirculo = Math.PI * radio * radio;
+        
+        System.out.println("El area de la figura es: " + areaCirculo);
+    }
 
-                		double areaCirculo = Math.PI * radio * radio;
-
-                		System.out.println("El area de la figura es: " + areaCirculo);
-
-                		break;
-            
-			case 3:	System.out.print("Introduce la base del triangulo: ");
-
-                		double base = scanner.nextDouble();
-
-                		System.out.print("Introduce la altura del triangulo: ");
-
-                		double altura = scanner.nextDouble();
-
-                		double areaTriangulo = (base * altura) / 2;
-
-                		System.out.println("El area de la figura es: " + areaTriangulo);
-
-                		break;
-
-            		default:
-System.out.println("Opcion no valida. Intentalo de nuevo.");
-
-        	}
-
-    	}
+    private static void calcularAreaCuadrado(Scanner scanner) {
+        System.out.print("Introduce el lado del cuadrado: ");
+        
+        double lado = scanner.nextDouble();
+        
+        double areaCuadrado = lado * lado;
+        
+        System.out.println("El area de la figura es: " + areaCuadrado);
+    }
 
 }
